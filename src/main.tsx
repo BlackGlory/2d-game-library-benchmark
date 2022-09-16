7a92be9c77dd852b0fe9b4150b979b252bac2ee0
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-09-16 13:42:57 +0000

    refactor: upgrade to React 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { App } from './app'
 import './index.css'
 
-ReactDOM.render(
+const container = document.querySelector('main')!
+const root = ReactDOM.createRoot(container)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-, document.querySelector('main')
 )
