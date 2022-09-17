3b630a79e270151ae95ce798d9296d655af85991
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-09-17 01:24:00 +0000

    refactor: use pixi

--- src/game.ts
+++ src/game.ts
@@ -4,6 +4,8 @@ import { World, Query, allOf } from 'extra-ecs'
 import { KeyStateObserver, Key, KeyState } from 'extra-key-state'
 import { random, randomInt, randomIntInclusive } from 'extra-rand'
 import { truncateArrayRight } from '@blackglory/structures'
+import { SyncDestructor } from 'extra-defer'
+import * as PIXI from 'pixi.js'
 import { COLORS } from './colors'
 
 const GAME_FPS = 60
@@ -12,11 +14,21 @@ const SCREEN_HEIGHT= 1080
 
 export function createGame(canvas: HTMLCanvasElement): GameLoop<number> {
   const fpsRecords: number[] = []
+  const entityIdToSprite = new Map<number, PIXI.Sprite>()
   const keyStateObserver = new KeyStateObserver(document.documentElement)
 
-  canvas.width = SCREEN_WIDTH
-  canvas.height = SCREEN_HEIGHT
-  const ctx = canvas.getContext('2d')!
+  PIXI.settings.RESOLUTION = window.devicePixelRatio
+  PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
+
+  const renderer = new PIXI.Renderer({
+    view: canvas
+  , width: SCREEN_WIDTH
+  , height: SCREEN_HEIGHT
+  , antialias: true
+  })
+  const stage = new PIXI.Container()
+  const particleStage = new PIXI.ParticleContainer(100000)
+  stage.addChild(particleStage)
 
   const world = new World()
 
@@ -28,9 +40,6 @@ export function createGame(canvas: HTMLCanvasElement): GameLoop<number> {
     x: double
   , y: double
   })
-  const Style = new StructureOfArrays({
-    color: uint8
-  })
   const Size = new StructureOfArrays({
     width: uint8
   , height: uint8
@@ -40,9 +49,9 @@ export function createGame(canvas: HTMLCanvasElement): GameLoop<number> {
   , y: double
   })
 
-  const queryBox = new Query(world, allOf(Position, Velocity, Size, Style))
+  const queryBox = new Query(world, allOf(Position, Velocity, Size))
 
-  let entities: number = 0
+  let boxes: number = 0
 
   const loop = new GameLoop({
     fixedDeltaTime: 1000 / GAME_FPS
@@ -54,7 +63,8 @@ export function createGame(canvas: HTMLCanvasElement): GameLoop<number> {
       directorSystem(deltaTime)
     }
   , render(alpha: number) {
-      renderingSystem(alpha)
+      updateStageSystem(alpha)
+      renderingSystem()
     }
   })
 
@@ -62,48 +72,53 @@ export function createGame(canvas: HTMLCanvasElement): GameLoop<number> {
 
   function movementSystem(deltaTime: number): void {
     for (const entityId of queryBox.findAllEntityIds()) {
-      savePreviousPosition(entityId)
+      updatePreviousPosition(entityId)
       Position.arrays.x[entityId] += Velocity.arrays.x[entityId] * deltaTime
       Position.arrays.y[entityId] += Velocity.arrays.y[entityId] * deltaTime
     }
   }
 
-  function savePreviousPosition(entityId: number): void {
-    PreviousPosition.arrays.x[entityId] = Position.arrays.x[entityId]
-    PreviousPosition.arrays.y[entityId] = Position.arrays.y[entityId]
+  function updatePreviousPosition(entityId: number): void {
+    const previousX = Position.arrays.x[entityId]
+    const previousY = Position.arrays.y[entityId]
+    PreviousPosition.upsert(entityId, {
+      x: previousX
+    , y: previousY
+    })
   }
 
   function directorSystem(deltaTime: number): void {
-    const oldEntities = entities
+    const oldEntities = boxes
     for (const entityId of queryBox.findAllEntityIds()) {
       if (
          keyStateObserver.getKeyState(Key.A) === KeyState.Down ||
          keyStateObserver.getKeyState(Key.Left) === KeyState.Down
       ) {
         Position.arrays.x[entityId] -= 1 * deltaTime
-        savePreviousPosition(entityId)
+        updatePreviousPosition(entityId)
       }
       if (
          keyStateObserver.getKeyState(Key.W) === KeyState.Down ||
          keyStateObserver.getKeyState(Key.Up) === KeyState.Down
       ) {
         Position.arrays.y[entityId] -= 1 * deltaTime
-        savePreviousPosition(entityId)
+        updatePreviousPosition(entityId)
       }
       if (
          keyStateObserver.getKeyState(Key.S) === KeyState.Down ||
          keyStateObserver.getKeyState(Key.Down) === KeyState.Down
       ) {
         Position.arrays.y[entityId] += 1 * deltaTime
-        savePreviousPosition(entityId)
+        updatePreviousPosition(entityId)
       }
       if (
          keyStateObserver.getKeyState(Key.D) === KeyState.Down ||
          keyStateObserver.getKeyState(Key.Right) === KeyState.Down
       ) {
         Position.arrays.x[entityId] += 1 * deltaTime
-        savePreviousPosition(entityId)
+        updatePreviousPosition(entityId)
       }
+
       const x = Position.arrays.x[entityId]
       const y = Position.arrays.y[entityId]
       const width = Size.arrays.width[entityId]
@@ -119,87 +134,121 @@ export function createGame(canvas: HTMLCanvasElement): GameLoop<number> {
     }
 
     if (loop.getFramesOfSecond() >= GAME_FPS) {
-      const removedEntities = oldEntities - entities 
+      const removedEntities = oldEntities - boxes
       for (let i = removedEntities + 1; i--;) {
         addBox()
       }
     }
   }
 
-  function renderingSystem(alpha: number): void {
-    ctx.clearRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)
-
-    ctx.save()
+  function updateStageSystem(alpha: number): void {
     for (const entityId of queryBox.findAllEntityIds()) {
-      const color = Style.arrays.color[entityId]
-      ctx.fillStyle = COLORS[color]
-      const x = lerp(alpha, PreviousPosition.arrays.x[entityId], Position.arrays.x[entityId])
-      const y = lerp(alpha, PreviousPosition.arrays.y[entityId], Position.arrays.y[entityId])
-      // const x = Position.arrays.x[entityId]
-      // const y = Position.arrays.y[entityId]
-      const width = Size.arrays.width[entityId]
-      const height = Size.arrays.height[entityId]
-      ctx.fillRect(x, y, width, height)
-    }
-    ctx.restore()
+      const previousX = PreviousPosition.arrays.x[entityId]
+      const previousY = PreviousPosition.arrays.y[entityId]
+      const currentX = Position.arrays.x[entityId]
+      const currentY = Position.arrays.y[entityId]
 
+      const rect = entityIdToSprite.get(entityId)!
+      rect.position.set(
+        lerp(alpha, previousX, currentX)
+      , lerp(alpha, previousY, currentY)
+      )
+    }
+  }
+
+  function renderingSystem(): void {
+    const destructor = new SyncDestructor()
     {
       fpsRecords.push(loop.getFramesOfSecond())
       truncateArrayRight(fpsRecords, GAME_FPS)
       const fps = Math.floor(fpsRecords.reduce((acc, cur) => acc + cur) / fpsRecords.length)
-      const text = `FPS: ${fps}`
-      ctx.save()
-      ctx.font = '48px sans'
-      ctx.textBaseline = 'top'
-      ctx.fillStyle = 'black'
-      const width = ctx.measureText(text).width
-      ctx.fillRect(0, 0, width, 48)
-      ctx.fillStyle = 'white'
-      ctx.fillText(text, 0, 0)
-      ctx.restore()
+
+      const text = new PIXI.Text(`FPS: ${fps}`, {
+        fontFamily: 'sans'
+      , fontSize: 48
+      , fill: 0xFFFFFF
+      })
+      destructor.defer(() => text.destroy())
+      text.position.x = 0
+      text.position.y = 0
+
+      const rect = new PIXI.Graphics()
+      destructor.defer(() => rect.destroy())
+      rect.beginFill(0x000000)
+      rect.drawRect(0, 0, text.width, text.height)
+      rect.position.x = text.position.x
+      rect.position.y = text.position.y
+
+      stage.addChild(rect, text)
+      destructor.defer(() => stage.removeChild(rect, text))
     }
 
     {
-      const text = `Entities: ${entities}`
-      ctx.save()
-      ctx.font = '48px sans'
-      ctx.textBaseline = 'top'
-      ctx.fillStyle = 'black'
-      const width = ctx.measureText(text).width
-      const x = SCREEN_WIDTH - width
-      ctx.fillRect(x, 0, width, 48)
-      ctx.fillStyle = 'white'
-      ctx.fillText(text, x, 0)
-      ctx.restore()
+      const text = new PIXI.Text(`Boxes: ${boxes}`, {
+        fontFamily: 'sans'
+      , fontSize: 48
+      , fill: 0xFFFFFF
+      })
+      destructor.defer(() => text.destroy())
+      text.position.x = SCREEN_WIDTH - text.width
+      text.position.y = 0
+
+      const rect = new PIXI.Graphics()
+      destructor.defer(() => rect.destroy())
+      rect.beginFill(0x000000)
+      rect.drawRect(0, 0, text.width, text.height)
+      rect.position.x = text.position.x
+      rect.position.y = text.position.y
+
+      stage.addChild(rect, text)
+      destructor.defer(() => stage.removeChild(rect, text))
     }
+
+    renderer.render(stage)
+
+    destructor.execute()
   }
 
   function addBox(): void {
-    const entityId = world.createEntityId()
     const x = random(0, SCREEN_WIDTH)
     const y = random(0, SCREEN_HEIGHT)
+    const vx = random(-0.01, 0.01)
+    const vy = random(-0.01, 0.01)
+    const width = randomIntInclusive(1, 100)
+    const height = randomIntInclusive(1, 100)
+    const colorIndex = randomInt(0, COLORS.length)
+
+    const entityId = world.createEntityId()
     world.addComponents(
       entityId
     , [Position, { x, y }]
-    , [Velocity, {
-        x: random(-0.01, 0.01)
-      , y: random(-0.01, 0.01)
-      }]
-    , [Size, {
-        width: randomIntInclusive(1, 100)
-      , height: randomIntInclusive(1, 100)
-      }]
-    , [Style, {
-        color: randomInt(0, COLORS.length)
-      }]
+    , [Velocity, { x: vx, y: vy }]
+    , [Size, { width, height }]
     )
+
+    const rect = new PIXI.Sprite(PIXI.Texture.WHITE)
+    rect.x = 0
+    rect.y = 0
+    rect.width = width
+    rect.height = height
+    rect.tint = COLORS[colorIndex]
+
     PreviousPosition.upsert(entityId, { x, y })
-    entities++
+
+    particleStage.addChild(rect)
+    entityIdToSprite.set(entityId, rect)
+
+    boxes++
   }
 
   function removeBox(entityId: number): void {
+    const rect = entityIdToSprite.get(entityId)!
+    rect.destroy()
+    particleStage.removeChild(rect)
     world.removeEntityId(entityId)
-    entities--
+    entityIdToSprite.delete(entityId)
+
+    boxes--
   }
 }
 
